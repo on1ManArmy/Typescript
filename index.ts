237360c92d04fa312c5e63bac0ff7ff7cc7e772a
Abhishek <iitjeeabhishek@gmail.com>
2024-10-22 16:35:29 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios
+  .get(url)
+  .then((res) => {
+    const todo = res.data as Todo;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+        The todo is: ${id}
+        Has a title: ${title}
+        Is it finished? ${completed}
+        `);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
